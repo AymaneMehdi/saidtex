5de700994c56f575bc4677625d0fd171680ac62f
Aymane Mehdi <unlocked.dark.web@gmail.com>
2024-05-22 03:53:37 +0100

    Update route.js

--- app/api/partners/route.js
+++ app/api/partners/route.js
@@ -5,14 +5,5 @@ import { NextResponse } from "next/server";
 export async function GET() {
   await connectMongoDB();
   const partners = await Partner.find();
-
-  return new Response(JSON.stringify(partners), {
-    status: 200,
-    headers: {
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
-      'Pragma': 'no-cache',
-      'Expires': '0',
-      'Vercel-CDN-Cache-Control': 'no-cache, no-store, must-revalidate',
-    },
-  });
+  return NextResponse.json( partners );
 }
