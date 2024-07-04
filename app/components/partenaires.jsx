b44190ccaf860c8f620b659513570911815204d0
Aymane Mehdi 🇵🇸 <unlocked.dark.web@gmail.com>
2024-07-05 00:35:30 +0100

    Update partenaires.jsx

--- app/components/partenaires.jsx
+++ app/components/partenaires.jsx
@@ -17,7 +17,7 @@ const Hero1 = () => {
 
   const fetchBlogs = async () => {
     try {
-      const response = await fetch("https://back-end-teal.vercel.app/partners");
+      const response = await fetch("https://back-end-olive-pi.vercel.app/partners");
       if (!response.ok) {
         throw new Error("Failed to fetch blogs");
       }
